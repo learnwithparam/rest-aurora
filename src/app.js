f43c4a4e46314cfb6697a49191ed04c2dcaf4bc2
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-01-06 21:17:19 +0200

    Fix eslint issues

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
 
 app.use(cors());
@@ -9,7 +10,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 
 // Routes
 app.use('/', (req, res) => {
-  res.json({ message: 'I am working!' })
+  res.json({ message: 'I am working!' });
 });
 
 module.exports = app;
