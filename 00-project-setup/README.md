6e41b49e883c1307fdd609ecdef33025c7ced060
Param Harrison <paramanantham@live.com>
2020-01-19 13:56:32 +0200

    feat: exercise ready for project structure

--- 00-project-setup/README.md
+++ 00-project-setup/README.md
@@ -13,3 +13,15 @@ Explain the project setup
 - [ ] Prettier
 - [ ] Docker
 - [ ] Dotenv
+
+## Task 02
+
+Return a Hello world message from root endpoint (i.e., `/` endpoint)
+
+When you visit `http://localhost:4000`, it should return a JSON output
+
+```json
+{
+  "message": "I am working! and you are connected at http://localhost:4000"
+}
+```
--- 00-project-setup/src/app.js
+++ 00-project-setup/src/app.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
-const app = express();
+const app = express(); // Create an express app instance
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
-app.use(bodyParser.json({ limit: '10mb' }));
-
-// Routes
 app.use('/', (req, res) => {
-  const { API_BASE_URL, PORT } = process.env;
-  res.json({
-    message: `I am working! and you are connected at ${API_BASE_URL}:${PORT}`
-  });
+  // TODO: Return a JSON data on root endpoint
 });
 
 module.exports = app;
