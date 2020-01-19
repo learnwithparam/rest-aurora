bc331807fd753366ebf0d86811df5aa974036969
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-01-19 14:46:05 +0200

    Validate access that owner can only do update and delete

--- src/tweets/routes.js
+++ src/tweets/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const { catchErrors } = require('../errorHandler');
+const { validTweetPost, isUserTweetOwner } = require('../validationHandler');
 const { authenticate, attachUserMiddleware } = require('../auth');
 
 const {
@@ -22,7 +23,7 @@ router.post('/', validTweetPost, catchErrors(postTweets));
 router.post('/batch', catchErrors(postBatchTweets));
 router.put('/batch', catchErrors(putBatchTweets));
 
-router.put('/:id', catchErrors(putTweets));
-router.delete('/:id', catchErrors(deleteTweets));
+router.put('/:id', isUserTweetOwner, catchErrors(putTweets));
+router.delete('/:id', isUserTweetOwner, catchErrors(deleteTweets));
 
 module.exports = router;
--- src/validationHandler.js
+++ src/validationHandler.js
@@ -1,3 +1,4 @@
+const Tweet = require('./tweets/model');
 const { badRequest } = require('./httpResponses');
 module.exports.validTweetPost = function(req, res, next) {
   const { body } = req;
@@ -15,5 +16,24 @@ module.exports.validUser = function(req, res, next) {
     badRequest(res, { message: 'Required field is missing' });
   }
 
+  next();
+};
+
+/**
+ * Middleware to check from authorization if the user is modifying
+ * it's tweet
+ */
+module.exports.isUserTweetOwner = async function(req, res, next) {
+  const { id } = req.params;
+  const { id: creatorId } = req.user || {};
+
+  try {
+    const tweet = await Tweet.findById(id).lean();
+    if (tweet.createdBy !== creatorId) {
+      throw new Error('Unauthorized to do request');
+    }
+  } catch (error) {
+    badRequest(res, { error: error.message });
+  }
   next();
 };
