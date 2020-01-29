1934127c30c846582785834fbd3c05c395cceec7
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-01-29 22:08:35 +0200

    Make log color approriate if info or error

--- src/httpLogger.js
+++ src/httpLogger.js
@@ -1,5 +1,11 @@
 const logger = require('./utils/logger');
 
 module.exports = require('express-pino-logger')({
-  logger
+  logger,
+  customLogLevel: res => {
+    if (res.statusCode >= 300 || res.statusCode < 200) {
+      return 'error';
+    }
+    return 'info';
+  }
 });
