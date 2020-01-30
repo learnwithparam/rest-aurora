d4973aafe9fc00c4eb1f0ffb89eb9a73d6855ad7
Param Harrison <paramanantham@live.com>
2020-01-30 19:56:15 +0200

    fix: project setup tasks

--- 00-project-setup/src/app.js
+++ 00-project-setup/src/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express(); // Create an express app instance
 
 app.use('/', (req, res) => {
-  // TODO: Return a JSON data on root endpoint
+  const { API_BASE_URL, PORT } = process.env;
+  res.json({
+    message: `I am working! and you are connected at ${API_BASE_URL}:${PORT}`
+  });
 });
 
 module.exports = app;
