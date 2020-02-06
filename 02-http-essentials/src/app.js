8148ba9720e65c927ef2cdcf762b2714c17ca51a
Param Harrison <paramanantham@live.com>
2020-02-06 20:53:06 +0200

    feat: http essentials solution

--- 02-http-essentials/src/app.js
+++ 02-http-essentials/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -18,44 +18,94 @@ const tweets = [
   }
 ];
 
+app.get('/show', (req, res) => {
+  res.json({
+    data: tweets
+  });
+});
+
 app.get('/get', (req, res) => {
-  // TODO 1: Display all messages
-  // TODO 2: Read from URL query params
+  const { id } = req.query;
+
+  if (!id) {
+    res.json({
+      error: 'undefined id'
+    });
+  }
+
+  if (id > tweets.length - 1) {
+    res.json({
+      error: "tweet doesn't exist"
+    });
+  }
+
+  res.json({
+    data: tweets[id]
+  });
 });
 
 app.post('/post', (req, res) => {
-  // TODO: Create new message
+  const { tweet } = req.body;
+  tweets.push({ text: tweet });
+  res.json({
+    data: tweet
+  });
 });
 
 app.put('/put/:id', (req, res) => {
-  // TODO 1: Read from url params
-  // TODO 2: Update a message based on the id received from URL
-  // TODO 3: Error response if ID not found
+  const { id } = req.params;
+  const { tweet } = req.body;
+
+  if (id > tweets.length - 1) {
+    res.json({
+      error: "tweet doesn't exist"
+    });
+  }
+
+  tweets[id] = { text: tweet };
+
+  res.json({
+    data: tweets[id]
+  });
 });
 
-app.delete('/', (req, res) => {
-  // TODO: Show the power of delete :)
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (id > tweets.length - 1) {
+    res.json({
+      error: "tweet doesn't exist"
+    });
+  }
+
+  tweets.splice(id, 1);
+
+  res.json({
+    data: `tweet at index ${id} removed`
+  });
 });
 
-/**
- * 
-Example HTML:
+app.get('/', (req, res) => {
+  const { format = 'json' } = req.query;
 
-<!DOCTYPE html>
-<html>
-  <body>
-    <h1>Hello Param</h1>
-    <p>Mr. Josh, How are you?</p>
-  </body>
-</html>
- */
-app.get('/html', (req, res) => {
-  // TODO: Send the above sample HTML
+  if (format === 'html') {
+    res.send(`<!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Hello Param</h1>
+        <p>Mr. Josh, How are you?</p>
+      </body>
+    </html>`);
+  }
+
+  res.json({ text: 'send json data' });
 });
 
 app.get('/custom-headers', (req, res) => {
-  // TODO 1: Set a custom header to the response
-  // TODO 2: Receive a custom header from request and console it
+  res.set('name', 'Josh');
+  res.json({
+    statusCode: res.statusCode
+  });
 });
 
 module.exports = app;
