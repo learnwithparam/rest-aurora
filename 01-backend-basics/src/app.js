4f56194a355139fb06c978617258b5312b32c657
Param Harrison <paramanantham@live.com>
2020-02-06 19:51:07 +0200

    feat: backend basics solution

--- 01-backend-basics/src/app.js
+++ 01-backend-basics/src/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 // Log the request time using express middleware
 app.use((req, res, next) => {
-  // TODO: Add the time to req.requestTime
+  req.requestTime = new Date().toISOString();
   next();
 });
 
-app.use('/', (req, res) => {
-  // TODO: Display the request time on root endpoint
+app.use('/', (req, res, next) => {
+  res.json({
+    data: {
+      requestTime: `${req.requestTime}`
+    }
+  });
 });
 
 module.exports = app;
